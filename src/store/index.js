cfa5a961aa8e6c81f61ec0ec311e5dfa8157ac38
Leonardo Nicola <leonardonicolares@gmail.com>
2022-10-19 11:56:39 -0300

    adicionado getter de id para componente viewer

--- src/store/index.js
+++ src/store/index.js
@@ -5,42 +5,48 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    card:{
-      name:'Leonardo Nicola',
-      number:'1239 1823',
-      balance:2031,
+    card: {
+      name: 'Leonardo Nicola',
+      number: '1239 1823',
+      balance: 2031,
     },
+    viewer: { id: 2 },
   },
   getters: {
-    cardInfos(state){
+    cardInfos(state) {
       return state.card
-    }
+    },
   },
-  mutations:{
-    addCardExpense(state, price){
+  mutations: {
+    addCardExpense(state, price) {
       state.card.balance -= price
       localStorage.setItem('cardBalance', JSON.stringify(state.card.balance))
     },
-    addFundsToCard(state, value){
+    addFundsToCard(state, value) {
       state.card.balance += value
       localStorage.setItem('cardBalance', JSON.stringify(state.card.balance))
-    }
+    },
+    changeViewerId(state, id) {
+      state.viewer.id = id
+    },
   },
-  actions:{
-      checkAccount({commit}, {expOrFund}){
-        console.log(expOrFund.value)
-        console.log(expOrFund.price)
-        //If the expense object return a value, it's from a add funds function
-        //If the expense object return a price, it's from a add expense function
-          if(expOrFund.value){
-            expOrFund.account == 'debitcard' ? commit('addFundsToCard', expOrFund.value) : commit('expenses/addFundsToWallet', expOrFund.value)
-          }else if(expOrFund.price){
-            expOrFund.account == 'debitcard' ? commit('addCardExpense', expOrFund.price) : null
-            commit('expenses/addExpense', {expense: expOrFund})
-          }
-
-          
+  actions: {
+    checkAccount({ commit }, { expOrFund }) {
+      console.log(expOrFund.value)
+      console.log(expOrFund.price)
+      //If the expense object return a value, it's from a add funds function
+      //If the expense object return a price, it's from a add expense function
+      if (expOrFund.value) {
+        expOrFund.account == 'debitcard'
+          ? commit('addFundsToCard', expOrFund.value)
+          : commit('expenses/addFundsToWallet', expOrFund.value)
+      } else if (expOrFund.price) {
+        expOrFund.account == 'debitcard'
+          ? commit('addCardExpense', expOrFund.price)
+          : null
+        commit('expenses/addExpense', { expense: expOrFund })
       }
+    },
   },
-  modules: {expenses}
+  modules: { expenses },
 })
